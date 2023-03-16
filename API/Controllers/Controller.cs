c8ea85df8fb575f3ffb319039240afc7ca1d5db8
Louis Devie <Louis_Devie@etu.u-bourgogne.fr>
2023-03-16 14:20:36 +0100

    Ajout du contrôleur de base

---
+++ API/Controllers/Controller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http.Json;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace GalliumPlusAPI.Controllers
+{
+    public class Controller : ControllerBase
+    {
+        private static JsonSerializerOptions jsonOptions;
+        public static JsonSerializerOptions JsonOptions { set => jsonOptions = value; }
+
+        public static IActionResult Json(object? value, int statusCode = StatusCodes.Status200OK)
+        {
+            return new ContentResult
+            {
+                Content = JsonSerializer.Serialize(value, jsonOptions),
+                ContentType = "application/json",
+                StatusCode = statusCode,
+            };
+        }
+
+        public static IActionResult Created() => new StatusCodeResult(StatusCodes.Status201Created);
+    }
+}
