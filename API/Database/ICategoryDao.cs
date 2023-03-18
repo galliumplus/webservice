7eccc2c4b0bdea98de4bdb7439136504c66a417a
Louis Devie <louis.devie@iut-dijon.u-bourgogne.fr>
2023-03-18 10:22:35 +0100

    Nettoyé la définition de `ICategoryDao`

--- API/Database/ICategoryDao.cs
+++ API/Database/ICategoryDao.cs
@@ -8,26 +8,5 @@ namespace GalliumPlusAPI.Database
     /// </summary>
     public interface ICategoryDao : IBasicDao<int, Category>
     {
-        
-        public void Create(Category product);
-
-        public IEnumerable<Category> ReadAll();
-
-        public Category ReadOne(int id);
-
-        /// <summary>
-        /// Modifie une catégorie.
-        /// </summary>
-        /// <param name="id">L'identifiant de la catégorie à modifier.</param>
-        /// <param name="category">Les nouvelles propriétés de la catégorie.</param>
-        /// <exception cref="ItemNotFoundException"></exception>
-        public void Update(int id, Category category);
-
-        /// <summary>
-        /// Supprime une catégorie. 
-        /// </summary>
-        /// <param name="id">L'identifiant de la catégorie à supprimer.</param>
-        /// <exception cref="ItemNotFoundException"></exception>
-        public void Delete(int id);
     }
 }
