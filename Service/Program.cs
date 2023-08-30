f1a8c3712e09971f37c3c699a21859cf1b5a67c8
AshotManukyan <69624036+Ashot-hex@users.noreply.github.com>
2023-08-30 16:48:12 +0200

    bump version

--- Service/Program.cs
+++ Service/Program.cs
@@ -110,7 +110,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
-ServerInfo.Current.SetVersion(0, 3, 1, "alpha");
+ServerInfo.Current.SetVersion(0, 3, 2, "alpha");
 Console.WriteLine(ServerInfo.Current);
 
 app.Run();
