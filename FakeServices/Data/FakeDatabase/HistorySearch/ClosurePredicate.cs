83837c225cf0453b29fe2cf5461d97092baae19f
Louis Devie <louis.devie@iut-dijon.u-bourgogne.fr>
2023-11-02 00:39:35 +0100

    Implémentation de la recherche dans la fause BDD

---
+++ FakeServices/Data/FakeDatabase/HistorySearch/ClosurePredicate.cs
@@ -0,0 +1,18 @@
+﻿using GalliumPlus.WebApi.Core.History;
+namespace GalliumPlus.WebApi.Data.FakeDatabase.HistorySearch
+{
+    internal class ClosurePredicate : IHistorySearchPredicate
+    {
+        private Predicate<HistoryAction> predicate;
+
+        public ClosurePredicate(Predicate<HistoryAction> predicate)
+        {
+            this.predicate = predicate;
+        }
+
+        public bool Matches(HistoryAction action)
+        {
+            return this.predicate.Invoke(action);
+        }
+    }
+}
