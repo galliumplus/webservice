6d4014a0ce94ecb301a613d047413c1a2803769c
Louis Devie <louis.devie@iut-dijon.u-bourgogne.fr>
2023-11-21 16:06:15 +0100

    correction du DAO de l'historique (récupération de l'heure)

--- Service/Program.cs
+++ Service/Program.cs
@@ -184,7 +184,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
-ServerInfo.Current.SetVersion(1, 0, 1, "beta");
+ServerInfo.Current.SetVersion(1, 0, 2, "beta");
 Console.WriteLine(ServerInfo.Current);
 
 app.Run();
