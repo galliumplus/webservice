11997ad899e7cf543964c89f646ddd9b5b7ab63e
Louis DEVIE <louisdevie.contact@gmail.com>
2024-10-16 21:47:00 +0200

    namespace réparé

--- WebService/Middleware/ServerInfoMiddleware.cs
+++ WebService/Middleware/ServerInfoMiddleware.cs
@@ -1,68 +1,72 @@
-﻿namespace GalliumPlus.WebService.Middleware;
-
-public class ServerInfoMiddleware : IMiddleware
+﻿namespace GalliumPlus.WebService.Middleware
 {
-    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    public class ServerInfoMiddleware : IMiddleware
     {
-        context.Response.Headers.Append("X-Gallium-Version", ServerInfo.Current.Version);
-        await next.Invoke(context);
-    }
-}
-
-public static class ServerInfoMiddlewareExtensions
-{
-    public static IServiceCollection AddServerInfo(this IServiceCollection services) =>
-        services.AddSingleton<ServerInfoMiddleware>();
-
-    public static IApplicationBuilder UseServerInfo(this IApplicationBuilder app) =>
-        app.UseMiddleware<ServerInfoMiddleware>();
-}
-
-public class ServerInfo
-{
-    private static ServerInfo? current = null;
-
-    public static ServerInfo Current
-    {
-        get
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
-            if (current == null)
-            {
-                current = new ServerInfo();
-            }
-            return current;
+            context.Response.Headers.Append("X-Gallium-Version", ServerInfo.Current.Version);
+            await next.Invoke(context);
         }
     }
 
-    private string version = $"unknown (unknown/{configuration})";
+    public static class ServerInfoMiddlewareExtensions
+    {
+        public static IServiceCollection AddServerInfo(this IServiceCollection services) =>
+            services.AddSingleton<ServerInfoMiddleware>();
+
+        public static IApplicationBuilder UseServerInfo(this IApplicationBuilder app) =>
+            app.UseMiddleware<ServerInfoMiddleware>();
+    }
+
+    public class ServerInfo
+    {
+        private static ServerInfo? current = null;
+
+        public static ServerInfo Current
+        {
+            get
+            {
+                if (current == null)
+                {
+                    current = new ServerInfo();
+                }
+
+                return current;
+            }
+        }
+
+        private string version = $"unknown (unknown/{configuration})";
 
 #if TEST
         private const string configuration = "test";
 #elif DEBUG
-    private const string configuration = "debug";
+        private const string configuration = "debug";
 #else
         private const string configuration = "release";
 #endif
 
-    public string Version => this.version;
+        public string Version => this.version;
 
 
-    private ServerInfo() { }
+        private ServerInfo()
+        {
+        }
 
-    public void SetVersion(int major, int minor, int patch, string stage = "unknown")
-    {
-        string build = Builtins.CompileDateTime.ToString("yyMMddHHmm");
-        this.version = $"{major}.{minor}.{patch}.{build} ({stage}/{configuration})";
-    }
+        public void SetVersion(int major, int minor, int patch, string stage = "unknown")
+        {
+            string build = Builtins.CompileDateTime.ToString("yyMMddHHmm");
+            this.version = $"{major}.{minor}.{patch}.{build} ({stage}/{configuration})";
+        }
 
-    public override string ToString()
-    {
-        return "   ___        _ _ _                   _    \n"
-               + "  / __|  __ _| | (_)_   _ _ __ ___  _| |_  \n"
-               + " | |  _ / _` | | | | | | | '_ ` _ \\'_   _|\n"
-               + " | |_| | (_| | | | | |_| | | | | | | |_|   \n"
-               + "  \\____|\\__,_|_|_|_|\\__,_|_| |_| |_|    \n"
-               + $"\n Gallium+ Web API Server v{this.Version}\n";
+        public override string ToString()
+        {
+            return "   ___        _ _ _                   _    \n"
+                   + "  / __|  __ _| | (_)_   _ _ __ ___  _| |_  \n"
+                   + " | |  _ / _` | | | | | | | '_ ` _ \\'_   _|\n"
+                   + " | |_| | (_| | | | | |_| | | | | | | |_|   \n"
+                   + "  \\____|\\__,_|_|_|_|\\__,_|_| |_| |_|    \n"
+                   + $"\n Gallium+ Web API Server v{this.Version}\n";
+        }
     }
 }
 
