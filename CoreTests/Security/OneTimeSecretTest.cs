3f8c8a3ba24650fac8d4f5ff2d98cfc1cff6d947
Louis DEVIE <louis.devie@kalico-informatique.fr>
2025-04-15 12:41:44 +0200

    test levée exception OneTimeSecret

---
+++ CoreTests/Security/OneTimeSecretTest.cs
@@ -0,0 +1,20 @@
+﻿using GalliumPlus.Core.Exceptions;
+using GalliumPlus.Core.Random;
+
+namespace GalliumPlus.Core.Security;
+
+public class OneTimeSecretTest
+{
+    [Fact]
+    public void NeverGenerated()
+    {
+        var ots = new OneTimeSecret();
+
+        Assert.Throws<NotBuildException>(() => ots.Hash);
+        Assert.Throws<NotBuildException>(() => ots.Salt);
+        Assert.Throws<NotBuildException>(() => ots.Match(""));
+
+        string secret = ots.Regenerate();
+        Assert.True(ots.Match(secret));
+    }
+}
