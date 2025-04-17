8a6a002654c0cdb7dd758acb448a23c4e47c4a42
Louis DEVIE <louisdevie.contact@gmail.com>
2025-04-17 18:11:32 +0200

    HashAndSaltMapper plus simple

--- External/Data/MariaDb/Implementations/HashAndSaltMapper.cs
+++ External/Data/MariaDb/Implementations/HashAndSaltMapper.cs
@@ -3,82 +3,52 @@ using GalliumPlus.Core.Security;
 using KiwiQuery.Mapped.Extension;
 using KiwiQuery.Mapped.Mappers.Fields;
 
-namespace GalliumPlus.Data.MariaDb;
+namespace GalliumPlus.Data.MariaDb.Implementations;
 
-[SharedMapper]
-public class HashAndSaltMapper : IFieldMapper
+public abstract class HashAndSaltMapper<T> : IFieldMapper
 {
-    public bool CanHandle(Type fieldType) => fieldType == typeof(PasswordInformation) || fieldType == typeof(OneTimeSecret);
+    public bool CanHandle(Type fieldType) => fieldType == typeof(T);
 
-    public IFieldMapper SpecializeFor(Type fieldType, IColumnInfo info, IFieldMapperCollection collection)
+    public IFieldMapper SpecializeFor(Type fieldType, IColumnInfo info, IFieldMapperCollection collection) => this;
+
+    public object? ReadValue(IDataRecord record, int offset)
     {
-        if (fieldType == typeof(PasswordInformation))
-        {
-            return new SpecializedForPasswordInformation();
-        }
-        else if (fieldType == typeof(OneTimeSecret))
-        {
-            return new SpecializedForOneTimeSecret();
-        }
-        else
-        {
-            throw new InvalidOperationException($"Les champs de type {fieldType.FullName} ne sont pas pris en charge.");
-        }
+        byte[] hash = new byte[32];
+        record.GetBytes(offset, 0, hash, 0, 32);
+        string salt = record.GetString(offset + 1);
+        return this.FromHashAndSalt(hash, salt);
     }
 
-    public object? ReadValue(IDataRecord record, int offset) =>
-        throw new InvalidOperationException("Ce mapper ne peut pas être utilisé sans spécialisation.");
-
     public IEnumerable<object?> WriteValue(object? fieldValue) =>
-        throw new InvalidOperationException("Ce mapper ne peut pas être utilisé sans spécialisation.");
+        [this.GetHash((T)fieldValue!), this.GetSalt((T)fieldValue!)];
 
     public IEnumerable<string> MetaColumns => ["salt"];
 
     public bool CanMapIntegerKey => false;
 
-    private abstract class Specialized<T> : IFieldMapper
-    {
-        public bool CanHandle(Type fieldType) => fieldType == typeof(T);
+    protected abstract T FromHashAndSalt(byte[] hash, string salt);
 
-        public IFieldMapper SpecializeFor(Type fieldType, IColumnInfo info, IFieldMapperCollection collection) => this;
+    protected abstract byte[] GetHash(T value);
 
-        public object? ReadValue(IDataRecord record, int offset)
-        {
-            byte[] hash = new byte[32];
-            record.GetBytes(offset, 0, hash, 0, 32);
-            string salt = record.GetString(offset + 1);
-            return this.FromHashAndSalt(hash, salt);
-        }
+    protected abstract string GetSalt(T value);
+}
 
-        public IEnumerable<object?> WriteValue(object? fieldValue) =>
-            [this.GetHash((T)fieldValue!), this.GetSalt((T)fieldValue!)];
+[SharedMapper]
+public class OneTimeSecretMapper : HashAndSaltMapper<OneTimeSecret>
+{
+    protected override OneTimeSecret FromHashAndSalt(byte[] hash, string salt) => new(hash, salt);
 
-        public IEnumerable<string> MetaColumns => ["salt"];
+    protected override byte[] GetHash(OneTimeSecret value) => value.Hash;
 
-        public bool CanMapIntegerKey => false;
+    protected override string GetSalt(OneTimeSecret value) => value.Salt;
+}
 
-        protected abstract T FromHashAndSalt(byte[] hash, string salt);
+[SharedMapper]
+public class PasswordInformationMapper : HashAndSaltMapper<PasswordInformation>
+{
+    protected override PasswordInformation FromHashAndSalt(byte[] hash, string salt) => new(hash, salt);
 
-        protected abstract byte[] GetHash(T value);
+    protected override byte[] GetHash(PasswordInformation value) => value.Hash;
 
-        protected abstract string GetSalt(T value);
-    }
-
-    private class SpecializedForOneTimeSecret : Specialized<OneTimeSecret>
-    {
-        protected override OneTimeSecret FromHashAndSalt(byte[] hash, string salt) => new(hash, salt);
-
-        protected override byte[] GetHash(OneTimeSecret value) => value.Hash;
-
-        protected override string GetSalt(OneTimeSecret value) => value.Salt;
-    }
-
-    private class SpecializedForPasswordInformation : Specialized<PasswordInformation>
-    {
-        protected override PasswordInformation FromHashAndSalt(byte[] hash, string salt) => new(hash, salt);
-
-        protected override byte[] GetHash(PasswordInformation value) => value.Hash;
-
-        protected override string GetSalt(PasswordInformation value) => value.Salt;
-    }
+    protected override string GetSalt(PasswordInformation value) => value.Salt;
 }
