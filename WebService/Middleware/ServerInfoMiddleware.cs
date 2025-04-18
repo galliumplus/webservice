92d14c7dc970f87d6ea104c3d2b45d9f6f09f89a
Louis DEVIE <louisdevie.contact@gmail.com>
2025-04-18 20:09:37 +0200

    fix: sorti la classe `Builtins` de son namespace

--- WebService/Middleware/ServerInfoMiddleware.cs
+++ WebService/Middleware/ServerInfoMiddleware.cs
@@ -1,72 +1,74 @@
-﻿namespace GalliumPlus.WebService.Middleware;
-
-public class ServerInfoMiddleware : IMiddleware
+﻿namespace GalliumPlus.WebService.Middleware
 {
-    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+
+    public class ServerInfoMiddleware : IMiddleware
     {
-        context.Response.Headers.Append("X-Gallium-Version", ServerInfo.Current.PrettyVersion);
-        await next.Invoke(context);
-    }
-}
-
-public static class ServerInfoMiddlewareExtensions
-{
-    public static IServiceCollection AddServerInfo(this IServiceCollection services) =>
-        services.AddSingleton<ServerInfoMiddleware>();
-
-    public static IApplicationBuilder UseServerInfo(this IApplicationBuilder app) =>
-        app.UseMiddleware<ServerInfoMiddleware>();
-}
-
-public class ServerInfo
-{
-    private static ServerInfo? current = null;
-
-    public static ServerInfo Current
-    {
-        get
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
-            if (current == null)
-            {
-                current = new ServerInfo();
-            }
-
-            return current;
+            context.Response.Headers.Append("X-Gallium-Version", ServerInfo.Current.PrettyVersion);
+            await next.Invoke(context);
         }
     }
 
-    private string prettyVersion = $"unknown (unknown/{CONFIGURATION})";
-    private string compactVersion = "unknown";
+    public static class ServerInfoMiddlewareExtensions
+    {
+        public static IServiceCollection AddServerInfo(this IServiceCollection services) =>
+            services.AddSingleton<ServerInfoMiddleware>();
+
+        public static IApplicationBuilder UseServerInfo(this IApplicationBuilder app) =>
+            app.UseMiddleware<ServerInfoMiddleware>();
+    }
+
+    public class ServerInfo
+    {
+        private static ServerInfo? current = null;
+
+        public static ServerInfo Current
+        {
+            get
+            {
+                if (current == null)
+                {
+                    current = new ServerInfo();
+                }
+
+                return current;
+            }
+        }
+
+        private string prettyVersion = $"unknown (unknown/{CONFIGURATION})";
+        private string compactVersion = "unknown";
 
 #if TEST
         private const string CONFIGURATION = "test";
 #elif DEBUG
-    private const string CONFIGURATION = "debug";
+        private const string CONFIGURATION = "debug";
 #else
         private const string CONFIGURATION = "release";
 #endif
 
-    public string PrettyVersion => this.prettyVersion;
-        
-    public string CompactVersion => this.compactVersion;
+        public string PrettyVersion => this.prettyVersion;
 
-    private ServerInfo() { }
+        public string CompactVersion => this.compactVersion;
 
-    public void SetVersion(int major, int minor, int patch, string stage = "unknown")
-    {
-        string build = Builtins.CompileDateTime.ToString("yyMMddHHmm");
-        this.prettyVersion = $"{major}.{minor}.{patch}.{build} ({stage}/{CONFIGURATION})";
-        this.compactVersion = $"{major}.{minor}.{patch}-{CONFIGURATION[0]}";
-    }
+        private ServerInfo() { }
 
-    public override string ToString()
-    {
-        return "   ___        _ _ _                   _    \n"
-               + "  / __|  __ _| | (_)_   _ _ __ ___  _| |_  \n"
-               + " | |  _ / _` | | | | | | | '_ ` _ \\'_   _|\n"
-               + " | |_| | (_| | | | | |_| | | | | | | |_|   \n"
-               + "  \\____|\\__,_|_|_|_|\\__,_|_| |_| |_|    \n"
-               + $"\n Gallium+ Web API Server v{this.PrettyVersion}\n";
+        public void SetVersion(int major, int minor, int patch, string stage = "unknown")
+        {
+            string build = Builtins.CompileDateTime.ToString("yyMMddHHmm");
+            this.prettyVersion = $"{major}.{minor}.{patch}.{build} ({stage}/{CONFIGURATION})";
+            this.compactVersion = $"{major}.{minor}.{patch}-{CONFIGURATION[0]}";
+        }
+
+        public override string ToString()
+        {
+            return "   ___        _ _ _                   _    \n"
+                   + "  / __|  __ _| | (_)_   _ _ __ ___  _| |_  \n"
+                   + " | |  _ / _` | | | | | | | '_ ` _ \\'_   _|\n"
+                   + " | |_| | (_| | | | | |_| | | | | | | |_|   \n"
+                   + "  \\____|\\__,_|_|_|_|\\__,_|_| |_| |_|    \n"
+                   + $"\n Gallium+ Web API Server v{this.PrettyVersion}\n";
+        }
     }
 }
 
